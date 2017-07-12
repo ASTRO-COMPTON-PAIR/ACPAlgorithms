2858cbf39aade1512ffed951049f15adb6fda1bb
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2017-07-12 17:36:04 +0200

    first

---
+++ code/ACPTelescopeConfig.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ ACPTelescopeConfig
+ -------------------
+ copyright            : (C) 2017 Andrea Bulgarelli, Alessio Aboudan
+ email                : bulgarelli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _ACPTELCONF_H
+#define _ACPTELCONF_H
+
+
+namespace ACPAlgorithm {
+	
+	class ACPTelescopeConfig {
+		
+	public:
+		int id;
+		
+	};
+}
+
+#endif
